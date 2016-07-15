c85815a121c4a051042cb4828ddad5d40ca55275
kiran paturi <kpaturi@smarttuition.com>
2016-07-15 20:59:59 +0530

    added code

---
+++ JavaScript/operators.js
@@ -0,0 +1,28 @@
+//matrimony
+
+var profiles = [{
+        name: "kiran",
+        profession: "Software",
+        income: 20000
+},
+    {
+        name: "John",
+        profession: "Business",
+        income: 100000
+
+              }];
+
+
+function selectProfile() {
+
+    for (var i = 0; i < profiles.length; i++) {
+
+
+        if (profiles[i].income >= 100000 && profiles[i].profession == "Business") {
+            console.log(profiles[i]);
+        }
+    }
+}
+
+
+selectProfile();
